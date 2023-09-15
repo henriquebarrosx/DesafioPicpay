13e39873a717a984c35ec93b57ed22d80fe53df2
Henrique Barros <henriquebarrosx@gmail.com>
2023-09-15 15:26:27 -0300

    substitui retorno no cadastro de usuário

--- src/main/java/com/picpay/bankapi/controllers/AccountController.java
+++ src/main/java/com/picpay/bankapi/controllers/AccountController.java
@@ -1,6 +1,5 @@
 package com.picpay.bankapi.controllers;
 
-import com.picpay.bankapi.entities.Account;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.picpay.bankapi.services.AccountService;
 import com.picpay.bankapi.controllers.DTOs.NewUserDTO;
+import com.picpay.bankapi.controllers.DTOs.AccountIdDTO;
 
 @Controller
 @AllArgsConstructor
@@ -18,9 +18,8 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<String> signUp(@RequestBody NewUserDTO params) {
+    public ResponseEntity<AccountIdDTO> signUp(@RequestBody NewUserDTO params) {
         var account = accountService.create(params);
-        var accountPath = "/api/account/v1/" + account.getId();
-        return ResponseEntity.status(HttpStatus.CREATED).body(accountPath);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new AccountIdDTO(account.getId()));
     }
 }
---
+++ src/main/java/com/picpay/bankapi/controllers/DTOs/AccountIdDTO.java
@@ -0,0 +1,10 @@
+package com.picpay.bankapi.controllers.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AccountIdDTO {
+    private Long id;
+}
