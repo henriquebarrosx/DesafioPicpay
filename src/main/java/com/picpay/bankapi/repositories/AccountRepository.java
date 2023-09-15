4c7198398879110df044e1ab1973e18c48400e4b
Henrique Barros <henriquebarrosx@gmail.com>
2023-09-15 16:11:22 -0300

    corrige consulta para verificar se já existe usuário com CPF/CNPJ ou email cadastrado

--- src/main/java/com/picpay/bankapi/repositories/AccountRepository.java
+++ src/main/java/com/picpay/bankapi/repositories/AccountRepository.java
@@ -1,10 +1,16 @@
 package com.picpay.bankapi.repositories;
 
 import com.picpay.bankapi.entities.Account;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> existsByCpfCnpjOrEmail(String cpfCnpj, String email);
+
+    @Query(value = "SELECT * FROM account acc WHERE acc.cpf_cnpj = :cpf_cnpj OR acc.email = :email", nativeQuery = true)
+    Optional<Account> existsByCpfCnpjOrEmail(
+            @Param("cpf_cnpj") String cpfCnpj,
+            @Param("email") String email);
 }
