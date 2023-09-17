644adb6519daaedc12e70ff9fb8a2557774d6260
Henrique Barros <henriquebarrosx@gmail.com>
2023-09-17 14:47:39 -0300

    refactor: simplifica métodos relacionados a criação de transações

--- src/main/java/com/picpay/bankapi/transaction/TransactionService.java
+++ src/main/java/com/picpay/bankapi/transaction/TransactionService.java
@@ -1,15 +1,17 @@
 package com.picpay.bankapi.transaction;
 
-import com.picpay.bankapi.account.AccountService;
 import com.picpay.bankapi.account.Account;
 import com.picpay.bankapi.email.EmailService;
+import com.picpay.bankapi.account.AccountService;
 import com.picpay.bankapi.account.AccountTypeEnum;
 import com.picpay.bankapi.exception.NotFoundException;
 import com.picpay.bankapi.exception.IllegalOperationException;
 
+import java.util.List;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class TransactionService {
     private final TransactionRepository transactionRepository;
+    private final TransactionBuilder transactionBuilder;
     private final AccountService accountService;
     private final EmailService emailService;
 
@@ -25,32 +28,44 @@ public class TransactionService {
         var payerAccount = accountService.findById(params.getPayerId());
         var payeeAccount = accountService.findById(params.getPayeeId());
 
-        var transaction = Transaction
-                .builder()
-                .value(params.getValue())
-                .payer(payerAccount)
-                .payee(payeeAccount)
-                .wasReversed(false)
-                .isChargeback(false)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
+        var transaction = transactionBuilder.buildDefaultTransaction(params.getValue(), payerAccount, payeeAccount);
+        validateTransactionCreation(transaction, payerAccount, payeeAccount);
 
-        validateTransaction(transaction, payerAccount, payeeAccount);
         accountService.subtractBalance(payerAccount, transaction.getValue());
         accountService.increaseBalance(payeeAccount, transaction.getValue());
 
-        var emailMessage = "You received a transaction of " + parseTransactionToCurrency(transaction.getValue());
+        var emailMessage = "You received a transaction of " + parseTransactionValueToCurrency(transaction.getValue());
         emailService.sendEmail(payeeAccount, emailMessage);
 
         return transactionRepository.save(transaction);
     }
 
-    String parseTransactionToCurrency(BigDecimal amount) {
+    String parseTransactionValueToCurrency(BigDecimal amount) {
         return NumberFormat.getCurrencyInstance().format(amount);
     }
 
-    void validateTransaction(Transaction transaction, Account payerAccount, Account payeeAccount) {
+    public void reverseTransaction(Long id) {
+        var transaction = getReversedTransaction(findById(id));
+
+        if (transaction.getWasReversed().equals(true) || transaction.getIsChargeback().equals(true)) {
+            throw new IllegalOperationException("The transaction cannot be reverted.");
+        }
+
+        var payerAccount = accountService.findById(transaction.getPayer().getId());
+        var payeeAccount = accountService.findById(transaction.getPayee().getId());
+        var chargeback = transactionBuilder.buildChargebackTransaction(transaction.getValue(), payeeAccount, payerAccount);
+
+        accountService.increaseBalance(payerAccount, transaction.getValue());
+        accountService.subtractBalance(payeeAccount, transaction.getValue());
+        transactionRepository.saveAll(List.of(transaction, chargeback));
+    }
+
+    public Transaction findById(Long id) {
+        return transactionRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Transaction %s not found.", id)));
+    }
+
+    void validateTransactionCreation(Transaction transaction, Account payerAccount, Account payeeAccount) {
         if (payerAccount.getId().equals(payeeAccount.getId())) {
             throw new IllegalOperationException("Payer and payee account should be different.");
         }
@@ -64,42 +79,9 @@ public class TransactionService {
         }
     }
 
-    public void reverseTransaction(Long id) {
-        var transaction = findById(id);
-
-        if (transaction.getWasReversed().equals(true) || transaction.getIsChargeback().equals(true)) {
-            throw new IllegalOperationException("The transaction cannot be reverted.");
-        }
-
+    Transaction getReversedTransaction(Transaction transaction) {
         transaction.setWasReversed(true);
         transaction.setUpdatedAt(LocalDateTime.now());
-        transactionRepository.save(transaction);
-
-        var payerAccount = accountService.findById(transaction.getPayer().getId());
-        var payeeAccount = accountService.findById(transaction.getPayee().getId());
-
-        accountService.increaseBalance(payerAccount, transaction.getValue());
-        accountService.subtractBalance(payeeAccount, transaction.getValue());
-        createChargebackTransaction(transaction.getValue(), payeeAccount, payerAccount);
-    }
-
-    void createChargebackTransaction(BigDecimal amount, Account payer, Account payee) {
-        var transaction = Transaction
-                .builder()
-                .value(amount)
-                .payer(payer)
-                .payee(payee)
-                .isChargeback(true)
-                .wasReversed(false)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        transactionRepository.save(transaction);
-    }
-
-    public Transaction findById(Long id) {
-        return transactionRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException(String.format("Transaction %s not found.", id)));
+        return transaction;
     }
 }
