9a3bf719f7de9c14d8e07e23fe7d1a26cf1b4b06
Henrique Barros <henriquebarrosx@gmail.com>
2023-09-21 11:57:21 -0300

    refactor: substitui implementação manual do método

--- src/main/java/com/picpay/bankapi/repository/AccountRepository.java
+++ src/main/java/com/picpay/bankapi/repository/AccountRepository.java
@@ -1,16 +1,10 @@
 package com.picpay.bankapi.repository;
 
 import com.picpay.bankapi.entity.Account;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
-    @Query(value = "SELECT * FROM account acc WHERE acc.cpf_cnpj = :cpf_cnpj OR acc.email = :email", nativeQuery = true)
-    Optional<Account> findByCpfCnpjOrEmail(
-            @Param("cpf_cnpj") String cpfCnpj,
-            @Param("email") String email);
+    Optional<Account> findByCpfCnpjOrEmail(String cpfCnpj, String email);
 }
